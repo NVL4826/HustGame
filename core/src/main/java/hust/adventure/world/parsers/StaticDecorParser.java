580076a6b3fd07fa5bd6ad376819509c15acce61
lan4826 <nguyenvietlan2006@gmail.com>
2026-06-06 19:53:00 +0700

    update outside map

--- core/src/main/java/hust/adventure/world/parsers/StaticDecorParser.java
+++ core/src/main/java/hust/adventure/world/parsers/StaticDecorParser.java
@@ -4,13 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
 import hust.adventure.entities.StaticObject;
 import hust.adventure.world.MapObjectParser;
 import hust.adventure.world.MapParseResult;
 
 /**
- * Parser for loading decorative tile objects (e.g. chairs, tables) from TMX layers.
+ * Parser for loading decorative tile objects (e.g. chairs, tables, trees) from TMX layers.
+ * Supports both Object Layers (TiledMapTileMapObject) and Tile Layers (TiledMapTileLayer).
  */
 public class StaticDecorParser implements MapObjectParser {
 
@@ -20,6 +23,42 @@ public class StaticDecorParser implements MapObjectParser {
             return;
         }
 
+        if (layer instanceof TiledMapTileLayer) {
+            parseTileLayer((TiledMapTileLayer) layer, result);
+        } else {
+            parseObjectLayer(layer, result);
+        }
+    }
+
+    private void parseTileLayer(final TiledMapTileLayer tileLayer, final MapParseResult result) {
+        final float tileWidth = tileLayer.getTileWidth();
+        final float tileHeight = tileLayer.getTileHeight();
+        int subZ = 0;
+
+        for (int x = 0; x < tileLayer.getWidth(); x++) {
+            for (int y = 0; y < tileLayer.getHeight(); y++) {
+                final Cell cell = tileLayer.getCell(x, y);
+                if (cell != null && cell.getTile() != null) {
+                    final TextureRegion region = cell.getTile().getTextureRegion();
+                    if (region != null) {
+                        final float worldX = x * tileWidth;
+                        final float worldY = y * tileHeight;
+
+                        final StaticObject decor = StaticObject.fromBottomLeft(worldX, worldY, tileWidth, tileHeight, region);
+                        decor.setSubZIndex(subZ++);
+                        if (tileLayer.getName() != null) {
+                            decor.setLayerName(tileLayer.getName());
+                        }
+                        result.addDecorEntity(decor);
+                    }
+                }
+            }
+        }
+        // Hide original layer so OrthogonalTiledMapRenderer doesn't double-render it statically
+        tileLayer.setVisible(false);
+    }
+
+    private void parseObjectLayer(final MapLayer layer, final MapParseResult result) {
         int subZ = 0;
         for (final MapObject obj : layer.getObjects()) {
             if (obj instanceof TiledMapTileMapObject) {
